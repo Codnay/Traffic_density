2e8a0a8a6d6c99a71c890ebe790511c5859388fb
Tushar2023 <58504958+Tushar2023@users.noreply.github.com>
2021-02-22 16:47:35 +0530

    Update point_selection.cpp

--- point_selection.cpp
+++ point_selection.cpp
@@ -6,6 +6,16 @@ using namespace cv;
 using namespace std;
 
 int main(){
-	//tform = projective2d(H);
-	//outputImage = imwarp(I, tform);
+	// pts_src and pts_dst are vectors of points in source
+	// and destination images. They are of type vector<Point2f>.
+	// We need at least 4 corresponding points.
+	Mat h = findHomography(pts_src, pts_dst);
+	// The calculated homography can be used to warp
+	// the source image to destination. im_src and im_dst are
+	// of type Mat. Size is the size (width,height) of im_dst.
+	//warpPerspective(im_src, im_dst, h, size);
+
+
+	tform = projective2d(h);
+	outputImage = imwarp(I, tform);
 }
