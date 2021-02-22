eaf089d7635f7ff7db9e514dd7496744bb376b41
Tushar2023 <58504958+Tushar2023@users.noreply.github.com>
2021-02-22 17:13:05 +0530

    Update point_selection.cpp

--- point_selection.cpp
+++ point_selection.cpp
@@ -5,7 +5,23 @@
 using namespace cv;
 using namespace std;
 
+vector<int> click_event(int event, int x, int y, int flag, void *param)
+{
+	if(event == EVENT_LBUTTONDOWN)
+	{
+		pt.x = x;
+		pt.y = y;
+		
+	}
+}	
 int main(){
+	
+	Mat I = imread("", 1);
+	imshow("image", I);
+	setMouseCallback("image", click_event);
+	waitkey(0);
+	destroyAllWindows();
+	
 	// pts_src and pts_dst are vectors of points in source
 	// and destination images. They are of type vector<Point2f>.
 	// We need at least 4 corresponding points.
