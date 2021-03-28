5910c82ce26356a5d42580d5eb25c5c10660656e
Tushar2023 <58504958+Tushar2023@users.noreply.github.com>
2021-03-29 00:29:41 +0530

    Update a1_part3_method4.cpp

--- 2019CS10383_2019CS10410_Part3/a1_part3_method4.cpp
+++ 2019CS10383_2019CS10410_Part3/a1_part3_method4.cpp
@@ -5,67 +5,65 @@
 #include <bits/stdc++.h>
 #include <vector>
 #include "helper.cpp"
-#include <thread>
+#include <pthread.h>
 
 using namespace cv;
 using namespace std;
 
 
-//This code would not work because we are passing by value but some
-//basic edits would fix this, would have to run and see
+int thread_tot= 4;
+ofstream fout ("out_method4_t4_2.txt");
+Mat I,O;
 
 
+struct thread_number_container
+{
+	int number;
+};
 
-void imgcalc(int thread_count, int thread_num){
-	
-
-	Mat I = imread("empty.jpg", IMREAD_GRAYSCALE);
-	Mat O = calc(I);
-
+void* imgcalc(void* arg){
 
 	vector<double> queue_density;
 	vector<int> frame_number;
 
 
+	struct thread_number_container *arg_struct = (struct thread_number_container*) arg;
 	//Note that this is hard coded for now, to change this 
 	//we will have to pass an extra string argument in imgcalc
 	string InputVideo= "trafficvideo.mp4";
 
 	int video_start= 0;
 
-	ofstream fout ("out.txt");
-	fout << "framenum" << "," << "queue density" <<"\n";
-
 	VideoCapture cap(InputVideo);
 	if(!cap.isOpened()){
 		cout << "Error loading the file"<< endl;
-		return;
+		exit(1);
 	}
 
 	while(1){
 
-		if(video_start%thread_count== thread_num){
+		if(video_start%thread_tot == arg_struct->number){
 
 
-		Mat frame,edges;
-		cap >> edges;
+			Mat frame,edges;
+			cap >> edges;
 
 
 
-		cvtColor(edges, frame, COLOR_BGR2GRAY);
-		//imshow("Frame", frame);
-		Mat out_frame;
-		//imshow("Frame", frame);
-		//waitKey(10);
-		out_frame= calc(frame);
-		//imshow("Frame", out_frame);
-		//waitKey(200);
+			cvtColor(edges, frame, COLOR_BGR2GRAY);
+			//imshow("Frame", frame);
+			Mat out_frame;
+			//imshow("Frame", frame);
+			//waitKey(10);
+			out_frame= calc(frame);
+			//imshow("Frame", out_frame);
+			//waitKey(200);
 
-		queue_density.push_back(density(out_frame, O, 25));
+			queue_density.push_back(density(out_frame, O, 25));
 
-		
+			
 
-		frame_number.push_back(video_start+1);
+			frame_number.push_back(video_start+1);
 
 		}
 
@@ -79,30 +77,42 @@ void imgcalc(int thread_count, int thread_num){
 	cap.release();
 	destroyAllWindows();
 
-	cout << "time (in secs)" << ", " << "queue density" <<"\n";
+	cout << "Thread :" << arg_struct->number << " finished" <<"\n";
 
 	for(int k = 0; k < frame_number.size(); k++){
 		//cout << frame_number[k]/15.0 << ", " << queue_density[k] << "\n";
 		fout << frame_number[k] << "," << queue_density[k]  << "\n";
 	}
 
-	fout.close();
-
+	pthread_exit(0);
 }
 
 int main(){
 	time_t start, end;
 	time(&start);
-
-	//Using 4 threads in our code
-	int thread_tot= 4;
-	vector<int> thread_count;
+	
+	/*vector<int> thread_count;
 
 	for(int i=0 ; i< thread_tot; i++){
 		thread_count.push_back(i);
+	}*/
+	struct thread_number_container args[thread_tot];
+
+	I = imread("empty.jpg", IMREAD_GRAYSCALE);
+	O = calc(I);
+
+	fout << "framenum" << "," << "queue density" <<"\n";
+
+	pthread_t tids[thread_tot];
+	for (int i = 0; i < thread_tot; ++i)
+	{
+		args[i].number = i;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&tids[i], &attr, imgcalc, &args[i]);
 	}
 
-	thread t1(imgcalc, thread_tot, thread_count[0]);
+	/*thread t1(imgcalc, thread_tot, thread_count[0]);
 	thread t2(imgcalc, thread_tot, thread_count[1]);
 	thread t3(imgcalc, thread_tot, thread_count[2]);
 	thread t4(imgcalc, thread_tot, thread_count[3]);
@@ -111,6 +121,15 @@ int main(){
 	t2.join();
 	t3.join();
 	t4.join();
+	*/
+
+	for (int i = 0; i < thread_tot; ++i)
+	{
+		pthread_join(tids[i], NULL);
+	}
+
+
+	fout.close();
 
 	time(&end);
 	double time_taken = double(end - start); 
