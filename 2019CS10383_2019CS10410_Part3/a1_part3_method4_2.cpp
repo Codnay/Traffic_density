36e70d5cf34457641e912f0b41602a9175d4ff9c
Codnay <62608401+Codnay@users.noreply.github.com>
2021-03-30 00:33:14 +0530

    Create a1_part3_method4_2.cpp

---
+++ 2019CS10383_2019CS10410_Part3/a1_part3_method4_2.cpp
@@ -0,0 +1,179 @@
+#include <stdio.h> 
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+//#include <bits/stdc++.h>
+#include <vector>
+#include "helper.cpp"
+#include <pthread.h>
+
+using namespace cv;
+using namespace std;
+
+
+int thread_tot= 3;
+
+vector<Mat> outstream;
+
+Mat I,O;
+
+struct thread_number_container
+{
+	int number;
+	vector<vector<double>> file_data;
+};
+
+void* imgcalc(void* arg){
+
+	struct thread_number_container *arg_struct = (struct thread_number_container*) arg;
+	//Note that this is hard coded for now, to change this 
+	//we will have to pass an extra string argument in imgcalc
+
+
+	int video_frame= 0;
+
+
+	while(video_frame < 5722){
+
+		if(video_frame%thread_tot == arg_struct->number){
+			vector<double> v;
+			v.push_back((double)video_frame+1);
+
+			v.push_back(density(outstream[video_frame], O, 25, thread_tot, arg_struct->number));
+
+			arg_struct->file_data.push_back(v);
+		}	
+
+
+		video_frame= video_frame+1;
+		
+		
+	}
+
+	cout << "Thread :" << arg_struct->number << " finished" <<"\n";
+
+	/*for(int k = 0; k < frame_number.size(); k++){
+		//cout << frame_number[k]/15.0 << ", " << queue_density[k] << "\n";
+		fout << frame_number[k] << "," << queue_density[k]  << "\n";
+	}*/
+
+	pthread_exit(0);
+}
+
+int main(){
+	time_t start, end;
+	time(&start);
+	
+	/*vector<int> thread_count;
+
+	for(int i=0 ; i< thread_tot; i++){
+		thread_count.push_back(i);
+	}*/
+	struct thread_number_container args[thread_tot];
+
+	I = imread("empty.jpg", IMREAD_GRAYSCALE);
+	O = calc(I);
+
+
+	string InputVideo= "trafficvideo.mp4";
+
+
+	int video_start= 0;
+
+	VideoCapture cap(InputVideo);
+	if(!cap.isOpened()){
+		cout << "Error loading the file"<< endl;
+		exit(1);
+	}
+
+	while(1){
+
+		
+		//cout << "Thread :" << arg_struct->number << " in process" <<"\n";
+		//cout<<"Framing number:   ";
+
+		Mat frame;
+		Mat edges;
+		cap >> edges;
+
+
+
+		cvtColor(edges, frame, COLOR_BGR2GRAY);
+		//imshow("Frame", frame);
+		Mat out_frame;
+		//imshow("Frame", frame);
+		//waitKey(10);
+		out_frame= calc(frame);
+		//imshow("Frame", out_frame);
+		//waitKey(200);
+
+		outstream.push_back(out_frame);
+
+
+		video_start= video_start+1;
+		char c= (char)waitKey(25);
+		if(c==27 || video_start == 5722){
+			break;
+		}
+		
+	}
+	cap.release();
+	destroyAllWindows();
+
+
+
+	pthread_t tids[thread_tot];
+	for (int i = 0; i < thread_tot; ++i)
+	{
+		args[i].number = i;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&tids[i], &attr, imgcalc, &args[i]);
+	}
+
+	/*thread t1(imgcalc, thread_tot, thread_count[0]);
+	thread t2(imgcalc, thread_tot, thread_count[1]);
+	thread t3(imgcalc, thread_tot, thread_count[2]);
+	thread t4(imgcalc, thread_tot, thread_count[3]);
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+	*/
+
+	for (int i = 0; i < thread_tot; ++i)
+	{
+		pthread_join(tids[i], NULL);
+	}
+
+
+
+
+	ofstream fout ("out_method3_thread3.txt");
+	fout << "framenum" << "," << "queue density" <<"\n";
+
+	vector<vector<double>> x= args[0].file_data;
+	
+	for (int j = 0; j < x.size(); ++j)
+	{	
+		double sum= 0;
+		for(int i=0; i<thread_tot; i++){
+
+			sum= sum+ args[i].file_data[j][1];
+		
+		}
+		sum= sum/ (O.rows*O.cols);
+
+		fout << x[j][0] << "," << sum << "\n";
+	}
+
+
+	fout.close();
+
+	time(&end);
+	double time_taken = double(end - start); 
+    cout << "Time taken by program is : " << fixed 
+         << time_taken << setprecision(5); 
+    cout << " sec " << endl;
+}
